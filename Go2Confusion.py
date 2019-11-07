2356c484851249fb1c45acc91b20eca65cbf1178
ucfilho <42356381+ucfilho@users.noreply.github.com>
2019-11-06 21:50:21 -0300

    Update Go2Confusion.py

--- Go2Confusion.py
+++ Go2Confusion.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 def Confusao(y,ypred):
+  y=np.array(y).ravel()
+  ypred=np.array(ypred).ravel()
   cm=np.array(pd.crosstab(y, ypred))
   TP = np.diag(cm) # true positive
   FP = np.sum(cm, axis=0) - TP # false positive
